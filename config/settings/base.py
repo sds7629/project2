217195d078e88216d04b5398ed58085223c2dac9
jinwoo <sds7629>
2023-09-14 16:59:58 +0900

    csrf_trust

--- config/settings/base.py
+++ config/settings/base.py
@@ -187,6 +187,9 @@ CORS_ORIGIN_WHITELIST = [
     "http://127.0.0.1:3000/",
     "http://localhost:3000/",
 ]
-
+CSRF_TRUSTED_ORIGINS = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+]
 # CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIAL = True
