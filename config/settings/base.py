0dd69fe3e03a0491b62bd55aa154e2c60daaa706
jinwoo <sds7629>
2023-09-14 18:05:08 +0900

    8000

--- config/settings/base.py
+++ config/settings/base.py
@@ -187,7 +187,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
-    "http://27.96.134.191",
+    "http://27.96.134.191:8000",
 ]
 # CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIAL = True
