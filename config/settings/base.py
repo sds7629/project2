b0ff8e84dc845042ca4ff0467c6d549f8c97d348
jinwoo <sds7629>
2023-09-14 16:50:43 +0900

    settings whitelist

--- config/settings/base.py
+++ config/settings/base.py
@@ -181,7 +181,12 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3001",
 ]
 
-CORS_ORIGIN_WHITELIST = ["http://127.0.0.1:3000", "http://localhost:3000"]
+CORS_ORIGIN_WHITELIST = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000/",
+    "http://localhost:3000/",
+]
 
 # CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIAL = True
